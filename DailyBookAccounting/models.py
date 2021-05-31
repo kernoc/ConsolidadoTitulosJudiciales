5fd6a70673978f0a2bf1d46341b0a95ff2437f26
leromac <leromac@hotmail.com>
2021-05-30 23:59:43 -0500

    Arreglo de error con campo IntegerField, se cambia por bitintegerField y se crea la clase JudicialTitle

--- DailyBookAccounting/models.py
+++ DailyBookAccounting/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
 class DailyBookAccounting(models.Model):
     idBook = models.AutoField("Id Interno", primary_key= True)
-    idPerson = models.IntegerField("Id Interno Persona", 20)
-    title= models.CharField("Titulo", max_length=30)
-    registrationDate= models.DateField("Fecha Registro")
-    registrationNumber= models.IntegerField("Numero Radicado", 20)
-    incomeValue = models.FloatField("Valor Ingreso")
-    outputValue = models.FloatField("Valor Egreso")
-    balance = models.FloatField("Saldo")
-    observations= models.CharField("Observaciones", max_length=50)
+    idPerson = models.BigIntegerField("Id Interno Persona")
+    title = models.CharField("Titulo", max_length=30)
+    registrationDate = models.DateTimeField("Fecha Registro", auto_now=False, auto_now_add=False)
+    registrationNumber = models.BigIntegerField("Numero Radicado")
+    incomeValue = models.DecimalField("Valor Ingreso", max_digits=19, decimal_places=2)
+    outputValue = models.DecimalField("Valor Egreso", max_digits=19, decimal_places=2)
+    balance = models.DecimalField("Saldo", max_digits=19, decimal_places=2)
+    observations = models.CharField("Observaciones", max_length=50)
+
+    def __str__(self):
+        return self.idBook
