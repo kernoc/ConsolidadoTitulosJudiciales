cefeac4b893f03ca500621b51f812e0280a76273
leromac <leromac@hotmail.com>
2021-05-30 20:57:11 -0500

    Definicion basica de la clase tipo de identificaciones

--- identificationType/models.py
+++ identificationType/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class identificationType(models.Model):
-    idIdentificationType = models.AutoField(primary_key= True)
-    codeIdentificationType = models.CharField(max_length=5)
-    descriptionIdentificationType = models.CharField(max_length=50)
+    idIdentificationType = models.AutoField("Id Interno", primary_key= True)
+    codeIdentificationType = models.CharField("Codigo Tipo Identificacion", max_length=5)
+    descriptionIdentificationType = models.CharField("Descripcion Tipo Identificacion", max_length=50)
 
     def __str__(self):
         return self.codeIdentificationType
